53ee1a1a7449640e18a2f9aa263ba980ea8d14cb
Alexander Nutz <alexandernutz68@gmail.com>
2025-01-05 21:00:40 +0100

    update dist script

--- dist.sh
+++ dist.sh
@@ -1,1 +1,17 @@
-tar -czf emenu.tar.gz $(git ls-files) $(cd slowdb/ && git ls-files | awk '{print "slowdb/"$1}')
+set -e
+if [ -z $1 ]; then
+    echo expected argument: version
+    exit 1
+fi
+mkdir emenu-$1
+files="$(git ls-files) $(cd slowdb/ && git ls-files | awk '{print "slowdb/"$1}')"
+for f in $files; do
+    if [ -f $f ]; then
+        if ! [ -z $(dirname "$f") ]; then
+            mkdir -p emenu-$1/$(dirname "$f")
+        fi
+        cp $f emenu-$1/$(dirname "$f")/
+    fi
+done
+tar -czf emenu-$1.tar.gz emenu-$1
+rm -r emenu-$1
